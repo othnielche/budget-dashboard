d9167502c08ee44172a756c977e8c6ff63486ee8
othnielche <cheothniel@gmail.com>
2025-02-07 09:36:12 +0100

    Updates on the create user component.

--- src/components/sidebar/subMenus/users/create-new-user.jsx
+++ src/components/sidebar/subMenus/users/create-new-user.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import React, { useState } from 'react'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from '@/components/ui/button'
+
 
 function CreateNewUser() {
+  const [role, setRole] = useState("Select Role")
+
   return (
-    <div>CreateNewUser</div>
+    <div>
+      <div className='font-medium text-xl mt-10'>
+        <text className=''> Create An Account For A New User</text>
+      </div>
+      <form>
+        <div className='flex flex-col mt-4 ml-2'>
+          <div className='flex flex-row '>
+              <div className='w-1/4'>
+                <Label htmlFor="Matricule">Matricule</Label>
+                <Input className='' id="Matricule" type="number" placeholder="matricule" required/>
+              </div>
+              <div className='ml-4 w-1/4 mr-4'>
+                <Label htmlFor="Name">Name</Label>
+                <Input className=' active:' id="Name" type="text" placeholder="name" required />
+              </div>
+          </div>
+          <div className='flex flex-row mt-4 '>
+              <div className='w-1/4'>
+                <Label htmlFor="Matricule">Group Code</Label>
+                <Input className='' id="GroupCode" type="text" placeholder="group code" required/>
+              </div>
+              <div className='ml-4 w-1/4 mr-4'>
+                <Label htmlFor="EstateCode">Estate Code</Label>
+                <Input className='' id="EstateCode" type="text" placeholder="estate code" required />
+              </div>
+          </div>
+          <div className='flex flex-row mt-4'>
+            <div className='w-1/4'>
+              <Label htmlFor="text">Password</Label>
+              <Input required className='' id="Password" type="password" placeholder="password" />
+            </div>
+          </div>
+          <div>
+              <text className='text-sm italic'>User has to update their password with one only they know *</text>
+          </div>
+          <div className='mt-4'>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant='outline'> Select User Role</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Select User Role</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuRadioGroup value={role} onValueChange={setRole}>
+                  <DropdownMenuRadioItem value="1">Administrator</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="2">Group Manager</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="3">Managing Controller (MC)</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value='4'>Estate Manager</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value='5'>Supplies Manager</DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value='6'>Head Office Manging Controller (HOMC)</DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <div className='flex ml-4 flex-row w-1/2 justify-end '>
+          <div className='flex mt-4 w-1/2 justify-end'>
+            <Button variant='' type='submit'>Create User</Button>
+          </div>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,7 +1,7 @@
 import {React, useState} from 'react';
 
 import AppSidebar from '@/components/app-sidebar';
-import { SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import ContentArea from '@/components/content-area';
 import { ScrollArea } from '@/components/ui/scroll-area';
 
@@ -17,13 +17,16 @@ export function Dashboard() {
             <div>
                 <SidebarProvider>
                     <AppSidebar onItemClick={handleItemClick} />
+                        <SidebarTrigger></SidebarTrigger>
                 </SidebarProvider>
             </div>
             <div className="flex-1 flex-col pl-4 overflow-y-auto">
-                <div className='flex flex-row'>
-                    <div className='flex-1 pt-1 flex-row h-10 text-xl font-semibold self-center text-left  top-0'>{activeItem?.title}</div>
-                    {/* Make div ocupy the remainder of the available viewport height and width*/ }
-                </div>
+                <header >
+                    <div className='flex flex-row'>
+                        <div className='flex-1 pt-1 flex-row h-10 text-xl font-semibold self-center text-left  top-0'>{activeItem?.title}</div>
+                        {/* Make div ocupy the remainder of the available viewport height and width*/ }
+                    </div>
+                </header>
                 <ScrollArea>
                     <div className='h-full w-full flex-grow bg-' >
                         <ContentArea activeItem={activeItem} />
